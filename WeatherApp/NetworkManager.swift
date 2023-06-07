86df8b4a2a08ad469af22e963f326b23c7bf6c55
Talor Levy <talorlv123@hotmail.com>
2023-06-07 16:27:37 -0700

    Finished MVVM migration and implemented NetworkManager singleton class

--- WeatherApp/NetworkManager.swift
+++ WeatherApp/NetworkManager.swift
@@ -12,7 +12,21 @@ class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
-    func fetchData<T: Decodable>(completion: @escaping(Result<T, Error>)->()) {
-        
+    func fetchData<T: Decodable>(url: URL, completion: @escaping(Result<T, Error>)->()) {
+        DispatchQueue.global().async {
+            URLSession.shared.dataTask(with: url) { data, _, error in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                guard let data = data else { return }
+                do {
+                    let model = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(model))
+                } catch (let error) {
+                    completion(.failure(error))
+                }
+            }.resume()
+
+        }
     }
 }
---
+++ WeatherApp/View Models/HomeViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewModel.swift
+//  WeatherApp
+//
+//  Created by Talor Levy on 6/7/23.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    var models: [Daily] = []
+    var currentWeather: Current?
+    var hourlyModels: [Current] = []
+    
+    func fetchWeatherData(lat: Double,
+                          long: Double,
+                          completion: @escaping(Result<Void, Error>)->()) {
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            guard let url = URL(string: Constants.generateWeatherApiUrl(lat: lat, long: long)) else {
+                return
+            }
+            NetworkManager.shared.fetchData(url: url) { (result: Result<WeatherResponse, Error>) in
+                switch result {
+                case .success(let weatherResponse):
+                    self?.models = weatherResponse.daily
+                    self?.currentWeather = weatherResponse.current
+                    self?.hourlyModels = weatherResponse.hourly
+                    completion(.success(()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
