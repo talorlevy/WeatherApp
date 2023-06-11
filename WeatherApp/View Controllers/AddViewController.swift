e0ca73c6f8744b40b05fd6f2d942410c30b6ba75
Talor Levy <talorlv123@hotmail.com>
2023-06-10 21:25:19 -0700

    Added Search and Add view controllers, both need to be completed

---
+++ WeatherApp/View Controllers/AddViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddViewController.swift
+//  WeatherApp
+//
+//  Created by Talor Levy on 6/10/23.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ WeatherApp/View Controllers/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  WeatherApp
+//
+//  Created by Talor Levy on 6/10/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
